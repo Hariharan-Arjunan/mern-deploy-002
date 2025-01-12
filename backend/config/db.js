813ec0ce0a674f855472d20a85bb5fe40fe9af3c
Hariharan-Arjunan <hariharanarjunan@gmail.com>
2025-01-12 06:18:21 +0530

    url

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("");
+    const conn = await mongoose.connect(
+      "mongodb+srv://hariharan19986:LtDgK3wAJC1oxRpk@cluster0.wvjlz.mongodb.net/products_db?retryWrites=true&w=majority&appName=Cluster0"
+    );
     console.log(`MongoDB Connected: ${conn?.connection?.host}`);
   } catch (error) {
     console.log(`Error : ${error?.message}`);
